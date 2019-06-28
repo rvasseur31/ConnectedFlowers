c23118f84e7927b07c2d7dd823bfcc57466d6eed
rvasseur31 <raphael.vasseur@ynov.com>
2019-06-28 12:43:59 +0200

    final product

--- nodeJs/main.js
+++ nodeJs/main.js
@@ -10,43 +10,31 @@ admin.initializeApp({
 
 utils.firstConnection();
 
-var data = '{"Temperature":26,"Luminosity":56,"Humidity":47,"Time":0}'
+//var data = '{"Temperature":26,"Luminosity":56,"Humidity":47,"Time":0}'
 
-try {
-    data = JSON.parse(data);
-    let today = new Date();
-    data.Time = today.getHours() + "h";
-    console.log(data);
-    utils.addDataToDatabase("Sensors", configUser.id, data);
-    utils.getCurrentFlower();
-    utils.checkData(data);
-} catch (err) {
-    console.log(err);
-}
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const port = new SerialPort('/dev/ttyACM0', { baudRate: 9600 });
+const parser = port.pipe(new Readline({ delimiter: '\n' }));
+// Read the port data
+port.on("open", () => {
+    console.log('serial port open');
+});
+parser.on('data', data => {
+    try {
+        data = JSON.parse(data);
+        let today = new Date();
+        data.Time = today.getHours() + "h";
+        console.log(data);
+        utils.addDataToDatabase("Sensors", configUser.id, data);
+        utils.getCurrentFlower();
+        utils.checkData(data);
+    } catch (err) {
+        console.log(err);
+    }
+});
 
-// const SerialPort = require('serialport');
-// const Readline = require('@serialport/parser-readline');
-// const port = new SerialPort('/dev/ttyACM0', { baudRate: 9600 });
-// const parser = port.pipe(new Readline({ delimiter: '\n' }));
-// // Read the port data
-// port.on("open", () => {
-//     console.log('serial port open');
-// });
-// parser.on('data', data => {
-//     try {
-//         data = JSON.parse(data);
-//         let today = new Date();
-//         data.Time = today.getHours() + "h";
-//         console.log(data);
-//         utils.addDataToDatabase("Sensors", configUser.id, data);
-//         utils.getCurrentFlower();
-//     } catch (err) {
-//         console.log(err);
-//     }
-// });
+// Test Functions
 
 // utils.checkData({"Temperature":24,"Luminosity":50,"Humidity":50,"Time":0});
-
-// utils.alertData();
-
 //utils.addDataToDatabase("Sensors", configUser.id, {"Temperature":24,"Luminosity":50,"Humidity":50,"Time":'12h'});
